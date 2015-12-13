d152e66e41115d201299aba77345b0645c41d7e6
Mario Cuba <mario@mariocuba.net>
2015-12-13 23:48:27 +1100

    Allow to find all by user ID in Accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,8 @@ class Account < ActiveRecord::Base
   
   # this should be removed if STI needs to be used
   self.inheritance_column = :unecessary
+
+  def self.find_all_by_user_id(user_id)
+    where(:user_id => user_id)
+  end
 end
