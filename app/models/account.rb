0ea267cb7e0c00246436d014fbd091fd7be2d9cf
Mario Cuba <mario@mariocuba.net>
2015-12-14 12:18:55 +1100

    Generate selects according to the account types

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,7 +4,22 @@ class Account < ActiveRecord::Base
   # this should be removed if STI needs to be used
   self.inheritance_column = :unecessary
 
+  def self.types
+    TYPES
+  end
+
   def self.find_all_by_user_id(user_id)
     where(:user_id => user_id)
   end
+
+  private
+
+  TYPES = [
+    'checkings',
+    'savings',
+    'credit_card',
+    'cash',
+    'paypal',
+  ]
+
 end
